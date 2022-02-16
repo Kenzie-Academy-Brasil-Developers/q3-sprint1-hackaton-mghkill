111642dd0c90d104c8b8a0cc20197a7b9b2f370f
Filipe Melo <filipexpert63@gmail.com>
2022-02-16 17:04:32 -0300

    .

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,13 @@
 # Seu código aqui
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+def get_score(team_name, teams):
+    for str in enumerate(teams):
+        if team_name == str[1]:
+            return f"A {str[1]} ficou classificada em {str[0] + 1}"
+            
+
+get_score("Team Ateliware", hackathon_1)
+
